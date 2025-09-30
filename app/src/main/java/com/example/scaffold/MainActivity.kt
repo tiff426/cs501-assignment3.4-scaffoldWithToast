4718eb2d1b88ff761ae61285e9cee77f90d52970
Tiffany Liu <mazingmonkey2004@gmail.com>
2025-09-30 17:35:39 -0400

    creating repo for completed project

---
+++ app/src/main/java/com/example/scaffold/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.scaffold
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.scaffold.ui.theme.ScaffoldTheme
+import kotlinx.coroutines.launch
+
+
+private const val TAG = "ScaffoldAssign"
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScaffoldTheme {
+                MainScaffold()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScaffold() {
+    val snackbarHostState = remember {SnackbarHostState()}
+    val snackbarScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("My App")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ) {
+                OutlinedButton(onClick = { Log.d(TAG, "home") }) {
+                    Text("home")
+                }
+                OutlinedButton(onClick = { Log.d(TAG, "settings") }) {
+                    Text("settings")
+                }
+                OutlinedButton(onClick = { Log.d(TAG, "profile") }) {
+                    Text("profile")
+                } }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    // liek what we did with animatescrolltoitem with scoping/coroutines
+                    snackbarScope.launch{snackbarHostState.showSnackbar("Feedback received!", actionLabel = "Dismiss")}
+                },
+                // Optional: Change the FAB's shape, color, etc.
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.secondary,
+                contentColor = MaterialTheme.colorScheme.onSecondary
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Email, // material icon
+                    contentDescription = "message"
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        content = { innerPadding -> // is this what they mean by applying inner padding
+            Column(modifier = Modifier.padding(innerPadding)) {
+                Text("hypothetically if there was more stuff on the page it would be in a composable and call it here")
+
+            }
+        }
+    )
+}
